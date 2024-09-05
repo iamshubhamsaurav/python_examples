ec9742599bdd9c193e6d05c6ebb87f9a90b2bd8e
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-09-05 19:50:16 +0530

    Added variables and datatypes file

---
+++ variables_datatypes.py
@@ -0,0 +1,25 @@
+#Differnt kind of value
+name = "Shubham Saurav"
+age = 120
+is_married = False
+
+any_dec_val = 23.324
+
+print("Name: ", name)
+print("Age: ", age)
+print("Is Married: ", is_married)
+print("Dec Value: ", any_dec_val)
+
+#User Input
+num = int(input("Enter a number: "))
+print("The number entered by user is : ", num)
+
+# Type Conversion
+float_num = float(input("Enter a decimal number: "))
+print("The decimal number entered by used is : ", float_num)
+
+num_str = str(num)
+float_num_str = str(float_num)
+
+print("Num in String: ", num_str)
+print("Dec Num in String: ", float_num_str)
