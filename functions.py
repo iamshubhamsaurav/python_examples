949e367a2627426db52a55367fbc76f71d136ac6
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-09-06 00:08:30 +0530

    added functions file

---
+++ functions.py
@@ -0,0 +1,12 @@
+def sum(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def greetSomeone(greeting, name):
+    return greeting + " " + name
+
+print(sum(12,13))
+print(sub(25,15))
+print(greetSomeone("Hello", "Shubham"))
