6d479e891ba7b1276102b10c1cad7829b8cd355a
Shubham  Saurav <shubhamsinha186@gmail.com>
2025-05-03 23:56:37 +0530

    Get, Post, Patch, Put, Delete route added

---
+++ mid_api/example.py
@@ -0,0 +1,7 @@
+from main import MidAPI
+
+app = MidAPI()
+
+@app.get('/app')
+def home_app():
+    return ""
--- mid_api/main.py
+++ mid_api/main.py
@@ -1,12 +1,55 @@
 from typing import Any
 
+# Internal Imports
+from mid_api.methods import Methods
+from response import Response
+
 class MidAPI:
     def __init__(self):
-        pass
+        # routes will store all the route as key
+        # the value will be a dictionary
+        # the key will be method and value will be handler
+        self.routes = {}
 
     def __call__(self, environ, start_response) -> Any:
-        print(environ)
-        start_response("200 OK", headers=[])
-        return [b"Hello, World"]
+        response = Response()
+        for path, handlers_dict in self.routes.items():
+            for request_method, handler in handlers_dict.items():
+                if environ['REQUEST_METHOD'] == request_method and path == environ['PATH']:
+                    handler(environ, response)
+                    response.as_wsgi(start_response)
+                    return [response.text.encode()]
     
-midapi = MidAPI()
+    def route_handler(self, path, handler, method):
+        path_name = path or f"{handler.__name__}"
+
+        if path_name not in self.routes:
+            self.routes[path_name] = {}
+            
+        self.routes[path_name][method] = handler
+        return handler
+
+    
+    def post(self, path=None):
+        def wrapper(handler):
+            return self.route_handler(path=path, handler=handler, method=Methods.POST.value)
+        
+        return wrapper
+    
+    def patch(self, path=None):
+        def wrapper(handler):
+            return self.route_handler(path=path, handler=handler, method=Methods.PATCH.value)
+        
+        return wrapper
+    
+    def put(self, path=None):
+        def wrapper(handler):
+            return self.route_handler(path=path, handler=handler, method=Methods.PUT.value)
+        
+        return wrapper
+    
+    def delete(self, path=None):
+        def wrapper(handler):
+            return self.route_handler(path=path, handler=handler, method=Methods.DELETE.value)
+        
+        return wrapper
